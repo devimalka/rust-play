4acd72d95ce75821296414f760c860b27ddeb957
devim <developerimalka@gmail.com>
2022-10-02 15:36:57 +0530

    minor Changes

--- src/files/mod.rs
+++ src/files/mod.rs
@@ -1,6 +1,7 @@
 use walkdir::{WalkDir};
 use dirs;
 
+
 pub mod get_files{
 
     use super::*;
@@ -12,7 +13,8 @@ pub mod get_files{
 
         for file in WalkDir::new(&home_dir).into_iter().filter_map(|file| file.ok()){
             if file.path().display().to_string().contains(".mp3"){
-                file_list.push(file.path().display().to_string());
+                 file_list.push(file.path().display().to_string());
+                // file_list.push(create_song_data(&file.path().display().to_string().to_string()));
             }
         }
 
@@ -21,6 +23,7 @@ pub mod get_files{
 
 
     
+            
+    }
 
    
-}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 
+
 #[allow(unused_imports)]
+#[allow(non_snake_case)]
+
 use rust_play::audio::playing;
 use rust_play::files::get_files;
+use audiotags::Tag;
+use std::io::ErroKind;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Song{
+    file_path:String,
+    title:String,
+    artist:String,
+    song_name:String,
+}
 
 fn main(){
 
@@ -10,13 +24,39 @@ fn main(){
     
   for file in files{
     println!("{}",file);
+    create_song_struct(&file);
+
   }
+  
 
 
 
 
+
+  
 }
 
 
 
+
+fn create_song_struct(file_path:&str) {
+
+  let tags = Tag::default().read_from_path(&file_path);
+
+
+  let tag = match tags{
+    Ok(tags) => tags,
+    Err(error) => match error.kind(){
+      ErroKind::Notag => println!("No tag found"),
+    }
+  };
+
+  // println!("{:?}",result);
+  // // let title = tags.title().unwrap_or("Not found");
+  // // let artist = tags.artist().unwrap_or("not found");
+  // // let track = tags.track().unwrap_or("not found");
+  
+
+  // println!("title:{:?}\nartist:{:?}\ntrack:{:?}",title,artist,track);
+}
 
